b01dbe1542442c7a47564dddef95c8aa88ffa47f
chengzichao <chengzichao@lansheng8.com>
2018-05-15 17:01:21 +0800

    修改canvas样式

--- js/index.js
+++
@@ -1,8 +0,0 @@
-let body = document.querySelector('body');
-let canvas = document.createElement('canvas');
-const WIDTH = window.innerWidth;
-const HEIGHT = window.innerHeight;
-canvas.width = WIDTH;
-canvas.height = HEIGHT;
-canvas.style.position = 'fixed';
-body.append(canvas);
